d9e303a41d13ab2a2afa2100f7d241b1dde4ccdc
Gonzalo Caudullo <gonchicaudullo@gmail.com>
2024-02-15 22:23:20 -0300

    ya implemente variables de entorno y commander, falta aquitectura por capas

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,23 @@
-// config.js
 import dotenv from 'dotenv';
-dotenv.config();
+import { Command } from 'commander'
+
+
+const program = new Command()
+
+program
+    .option('--mode <mode>', 'Este es el modo de ejecución del Backend', 'development')
+    .parse();
+
+const mode=program.opts().mode;
+console.log('mode', mode)
+let path;
+if (mode==='production') {
+    path = '.env.production'
+} else {
+    path = '.env.development'
+}
+
+dotenv.config({ path });
 
 export default {
     port: process.env.PORT || 3000,
